47600623032c0aaf17b25cb82e37029e70d12ec2
Arhian Albis Ramos <108996128+Arhian99@users.noreply.github.com>
2022-10-14 11:38:58 -0500

    working calculator, logs numbers and operators into the screen and the result, able use the result of one operation as the first value of another operation and also supports operations stringed together such as 7 + 2 - 3 * 7, however does not follow PEMDAS instead evaluates left to right

---
+++ index.js
@@ -0,0 +1,190 @@
+let clear = document.getElementById('clear');
+let division = document.getElementById('division');
+let seven = document.getElementById('seven');
+let eight = document.getElementById('eight');
+let nine = document.getElementById('nine');
+let multiplication = document.getElementById('multiplication');
+let four = document.getElementById('four');
+let five = document.getElementById('five');
+let six = document.getElementById('six');
+let subtraction = document.getElementById('subtraction');
+let one = document.getElementById('one');
+let two = document.getElementById('two');
+let three = document.getElementById('three');
+let addition = document.getElementById('addition');
+let zero = document.getElementById('zero');
+let decimal = document.getElementById('decimal');
+let equal = document.getElementById('equal');
+let screenText = document.getElementById('screen-text');
+
+let numberA;
+let numberB;
+let operator;
+
+let numbers = document.getElementsByClassName('numbers'); //recall this is an array//node list 
+let operators = document.getElementsByClassName('operator');
+
+//math functions
+function add(numberA, numberB) {
+    return +numberA + +numberB;
+}
+
+function subtract(numberA, numberB) {
+    return +numberA - +numberB;
+}
+
+function multiply(numberA, numberB) {
+    return +numberA * +numberB;
+}
+
+function divide(numberA, numberB) {
+    return +numberA / +numberB;
+}
+
+
+//operate function
+
+function operate(numberA, operator, numberB) {
+    if (operator === '+') {return add(numberA, numberB)}
+    else if (operator == '-') {return subtract(numberA, numberB)}
+    else if (operator == '*') {return multiply(numberA, numberB)}
+    else if (operator == '/') return divide(numberA, numberB)
+}
+
+/*fix the fact that the operator must be in string form (in quotations)
+for this to work and must be separated by commas */
+
+//screen population functions
+
+// changes the text on the screen
+//screenText.innerHTML(number.textContent());
+
+// adds event listener to each number so that upon click its value will be logged on the screen
+
+/*
+function eventListeners(numbers) {
+    () => numbers.forEach(number => {
+        number.addEventListener('click', number => screenText.innerHTML(number.textContent()))
+    })
+}
+
+eventListeners(numbers);
+*/
+
+/*
+function handler(event) {
+    let text = [];
+    text.push(event.target.textContent);
+    let onScreen = text.join('');
+    screenText.innerHTML = onScreen;
+}
+*/
+
+let onScreenA = ''; //stores the value on the screen (to be transferred to NumberA after operator is pressed or NumberB after equal is pressed)
+let num = [];
+let typed = '';
+
+for (number of numbers) {
+    number.addEventListener('click', event => {
+        onScreenA += event.target.textContent;
+        screenText.innerHTML = onScreenA;        
+
+        typed += event.target.textContent;
+    })
+};
+
+for (operator of operators) {
+    operator.addEventListener('click', event => {
+
+        if (num[0] == undefined) {
+            onScreenA += event.target.textContent;
+            screenText.innerHTML = onScreenA;     
+
+            num.push(typed);
+            num.push(event.target.textContent);
+            typed = '';
+        }
+
+        else {
+            onScreenA += event.target.textContent;
+            screenText.innerHTML = onScreenA;
+            operator = event.target.textContent;
+            num.push(operator);
+        }
+/*
+        if (num[0] == undefined) {
+            num[0] = typed;
+            num[1] = event.target.textContent; //sets the operator
+            typed = '';
+        }
+*/
+    }) 
+}
+
+
+equal.addEventListener('click', event => {
+    num.push(typed);
+
+
+    numberA = num[0];
+    operator = num[1];
+    numberB = num[2];
+    num.splice(0, 3);
+    num.unshift(operate(numberA, operator, numberB));
+
+    if (num.length != 1) {
+        do {
+            numberA = num[0];
+            operator = num[1];
+            numberB = num[2];
+            num.splice(0, 3);
+            num.unshift(operate(numberA, operator, numberB));
+        }
+        while (num.length !== 1)
+    }
+    
+    onScreenA = num[0];
+    screenText.innerHTML = onScreenA;
+    typed = '';
+})
+
+
+clear.addEventListener('click', event => {
+    onScreenA = '';
+    screenText.innerHTML = onScreenA;
+    typed = '';
+    num = [];
+})
+
+/*
+for (operator of operators) {
+    operator.addEventListener('click', event => {
+        for (let i = 0; i <= num.length; i =+ 2) {
+            if (num[i] ==  undefined) {
+                num[i] = onScreenA;
+                num[i + 1] = event.target.textContent;
+                textA.push(event.target.textContent);
+                onScreenA = textA.join('');
+                screenText.innerHTML = onScreenA;
+                textA = [];
+            }
+        }
+        
+        
+
+    })
+}
+
+*/
+
+
+/*
+numberA = onScreenA;
+operator = event.target.textContent;
+screenText.innerText = `${operator}`;
+*/
+
+/* event => screenText.innerHTML = event.target.textContent
+event => text.push(event.target.textContent)*/
+
+
